887b6200adc81cc42d24d456f51fd9219c628dc0
cdrcrmss <cedricramos52@gmail.com>
2025-11-18 16:40:43 +0800

    Fix light mode to turn entire UI white including all backgrounds and cards

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-black light:bg-white">
+    <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black light:bg-white text-white light:text-gray-900 transition-colors duration-300`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased transition-colors duration-300`}
       >
         {children}
       </body>
