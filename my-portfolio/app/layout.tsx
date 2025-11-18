3a42e2014a2a48e02f5e107c10f3283230119f4f
cdrcrmss <cedricramos52@gmail.com>
2025-11-18 11:50:48 +0800

    Ensure complete theme switching for entire page background

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-black light:bg-white">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black light:bg-white text-white light:text-gray-900 transition-colors duration-300`}
       >
